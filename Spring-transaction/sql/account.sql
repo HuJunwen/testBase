6c492a11a1b3e04ffca1df116d17d04059d93400
Your Name <329357842@qq.com>
2017-07-31 23:12:58 +0800

    这是linux下的一次提交

--- Spring-transaction/sql/account.sql
+++ Spring-transaction/sql/account.sql
@@ -7,4 +7,3 @@ CREATE TABLE `account` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 INSERT INTO `account` VALUES ('1', 'aaa', '1000');
 INSERT INTO `account` VALUES ('2', 'bbb', '1000');
-INSERT INTO `account` VALUES ('4', 'ccc', '1000');
